310ceaedde7fa85c467a05b23ffba9b524fc7a56
mayor04 <mayorwa4@gmail.com>
2022-03-25 11:30:56 +0100

    remove comment

--- lib/features/view/home/input_widgets/input_map.dart
+++ lib/features/view/home/input_widgets/input_map.dart
@@ -5,23 +5,7 @@ import 'package:flutter_widget_builder/features/fwb/fwb_input/fb_inputs.dart';
 import 'package:flutter_widget_builder/features/view/home/input_widgets/group_input.dart';
 import 'package:flutter_widget_builder/features/view/home/input_widgets/input_widget.dart';
 
-// typedef InputMapCallback = Widget Function(FbInputBase, VoidCallback);
-// typedef GroupInputMapCallback = Widget Function(FbGroupInputBase, VoidCallback);
-
 class InputItemBox {
-  // static final Map<FbInputType, InputMapCallback> input = {
-  //   FbInputType.small: (FbInputBase inputData, VoidCallback onEditComplete) =>
-  //       InputSmall(
-  //         smallInputData: inputData.cast<FbInputDataSmall>(),
-  //         onEditComplete: onEditComplete,
-  //       ),
-  //   FbInputType.color: (FbInputBase inputData, VoidCallback onEditComplete) =>
-  //       InputColor(
-  //         colorInputData: inputData.cast<FbInputDataColor>(),
-  //         onEditComplete: onEditComplete,
-  //       ),
-  // };
-
   static Widget getInput({
     required FbInputBase inputData,
     required VoidCallback onEditComplete,
@@ -56,13 +40,4 @@ class InputItemBox {
         return const SizedBox();
     }
   }
-
-  // static final Map<FbGroupType, GroupInputMapCallback> group = {
-  //   FbGroupType.smallHW:
-  //       (FbGroupInputBase fbInputGroup, VoidCallback onEditComplete) =>
-  //           GroupInputHW(
-  //             fbGroupData: fbInputGroup.cast<FbGroupHWData>(),
-  //             onEditComplete: onEditComplete,
-  //           ),
-  // };
 }
