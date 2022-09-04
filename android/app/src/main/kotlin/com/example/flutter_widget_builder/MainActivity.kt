a6fbe8aefacadf29a7e444db59d7862f104e69e5
Mayor04 <mayorwa4@gmail.com>
2022-09-04 19:55:39 +0100

    fix: deleted android folder

--- android/app/src/main/kotlin/com/example/flutter_widget_builder/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_widget_builder
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- fb_application/android/app/src/main/kotlin/com/blinp/fb_application/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.blinp.fb_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
